7faa87da81de812de1e3644384c12388bbbd15c1
mallika <mallikarathi151@gmail.com>
2026-07-24 09:55:01 +0530

    Add project files

---
+++ index.js
@@ -0,0 +1,28 @@
+// console.log("Hello Using 35")
+//  var a = 89;
+//  console.log("a=" +a);
+
+
+// var a = 78;
+// if (a >50){
+//     var a = 90;
+//     console.log("a=" +a);
+// }
+// console.log("a=" +a);
+
+//arrow function
+// const msg = (mymsg)=>{
+//     console.log("hello"+mymsg);
+
+// }
+// msg("wlcm to fsd");
+
+
+//  const sqrt=(num)=>{
+//     return Math.sqrt(num);
+//  }
+//  console.log(sqrt(25));
+
+
+//IIFE
+(()=>{console.log("hello....iife")})();
