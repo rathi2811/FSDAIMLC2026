08e9bdbecf4b0c87cb30f8b26e0b4ca72cbc29cd
mallika <mallikarathi151@gmail.com>
2026-08-20 10:18:08 +0530

    Table formation

--- UseFetch.js
+++ UseFetch.js
@@ -1,13 +1,39 @@
-const container = document.getElementById('container');
-const button = document.getElementById('button');
-console.log(container);
-function display(){
-    const serverdata =fetch("https://www.abes.ac.in/");
-    const jsondata = srverdata.json();
-    console.log(serverdata);
-    console.log(jsondata);
+const div=document.getElementById('container')
+const button=document.getElementById('btn')
+// console.log(div);
+const h2 = document.createElement('h2');
+h2.innerText = "Data is loading.....";
+console.log(h2);
+function addToCart(Item_ID){
+    console.log("Item added to cart with ID:", Item_ID);
+    alert("Item is added to cart ")
+}
+async function display(){
+    try{
+        // div.innerHTML='<h2>Hello Using DOM</h2>';
+        div.appendChild(h2);
+        const serverdata=await fetch('https://fakestoreapi.com/products');
+        const jsonData= await serverdata.json();
+        // console.log(jsonData[1].title)
+        // div.innerHTML=`<h2 style="color: blue;">${JSON.stringify(jsonData)}</h2>`;
+
+        let table=`<table border=4px>
+        <tr><th>Image</th><th>Item_ID</th><th>Item_Title</th><th>Price</th><th>CART</th></tr>
+        ${jsonData.map((ele) =>(
+        `<tr>
+        <td><img src =${ele.image} widht=50 height=200></td>
+        <td>${ele.id}</td>
+        <td>${ele.title}</td>
+        <td>$${ele.price}</td>
+        <td><button onclick="addToCart(${ele.id})">Add to Cart</button></td>
+        </tr>`))
+        }
+        </table>`
+        div.innerHTML = table;        // div.innerHTML=`<h2 style="color: blue;">${JSON.stringify(jsonData)}</h2>`;
+
+    } catch (e) {
+        console.log('Error fetching data:', e);
+    }finally{
+}
 }
 button.addEventListener('click',display);
-// container.innerHTML = '<h1>hello DOM</h1>';
-// container.style.color = 'red';
-// container.style.backgroundColor = 'yellow';
