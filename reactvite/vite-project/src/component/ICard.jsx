209cb0a176d8fc732878da8e85acecfea733bc1b
rathi2811 <mallikarathi151@gmail.com>
2026-09-10 09:12:30 +0530

    new project

---
+++ reactvite/vite-project/src/component/ICard.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+function ICard() {
+  let a=23;
+  let name="Ram"
+  return (
+    <div>
+      <h2 style={{backgroundColor:'brown',color:'white'}}>ABES ICard</h2>
+      <h2 style={{color:'red'}}>Welcome to react using vite</h2>
+      <h2 style={{color:'red'}}>Value of a={a}</h2>
+      <h2 style={{color:'red'}}>Name={name}</h2>
+      </div>
+  )
+}
+
+export default ICard
