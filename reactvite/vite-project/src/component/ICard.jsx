54d17da3b54b44d24f2e5f4df2567dbdaa4bed5b
rathi2811 <mallikarathi151@gmail.com>
2026-09-11 10:21:41 +0530

    ICardGallery

--- reactvite/vite-project/src/component/ICard.jsx
+++ reactvite/vite-project/src/component/ICard.jsx
@@ -1,15 +1,17 @@
 
 import React from 'react'
 
-function ICard() {
-  let a=23;
-  let name="Ram"
+function ICard(props) {
   return (
-    <div>
-      <h2 style={{backgroundColor:'brown',color:'white'}}>ABES ICard</h2>
-      <h2 style={{color:'red'}}>Welcome to react using vite</h2>
-      <h2 style={{color:'red'}}>Value of a={a}</h2>
-      <h2 style={{color:'red'}}>Name={name}</h2>
+    <div style={{border:'2px solid black',height:'2px',width:'200%',margin:'10px'}}> 
+
+      <h2 style={{backgroundColor:'brown',color:'white'}}>ABES Engineering College</h2>
+      {/* <h2 style={{color:'red'}}>Welcome to react using vite</h2> */}
+      <img src={props.pic} height={200} width={200} ></img>
+      <h3 style={{color:'red'}}>Value of name={props.name}</h3>
+      <h3 style={{color:'red'}}>Roll Number={props.rollNumber}</h3>
+      <h3 style={{color:'red'}}>Branch={props.branch}</h3>
+
       </div>
   )
 }
---
+++ reactvite/vite-project/src/component/ICardGallery.jsx
@@ -0,0 +1,13 @@
+ import React from 'react'
+ import ICard from './ICard'
+
+function ICardGallery() {
+  return (
+    <div>
+    <ICard  pic = {mypic} roll ='118' name ="Mallika" branch="CSE-Aiml" />
+
+    </div>
+  )
+}
+
+export default ICardGallery
